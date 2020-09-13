457389328843ce9a8529a8cd73abb0ee9a19e594
hui-home-desktop <yihaozhadan@gmail.com>
2020-09-13 10:44:12 -0700

    Add guru99.com

---
+++ js/website/guru99.com.js
@@ -0,0 +1,4 @@
+$("#g-aside, #div-gpt-ad-9092914-1, #div-gpt-ad-9092914-2, .top-ads-boxes").remove();
+// Has Attribute Selector [name]
+// https://api.jquery.com/has-attribute-selector/
+$("div[data-google-query-id]").remove(); // It doesn't work
