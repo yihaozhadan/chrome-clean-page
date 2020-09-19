dc6c7b6e089ba98a8e00c565da18b0832ef401fe
hui-home-desktop <yihaozhadan@gmail.com>
2020-09-18 20:46:22 -0700

    Update PlantUML website

--- js/website/guru99.com.js
+++ js/website/guru99.com.js
@@ -1,4 +1,3 @@
 $("#g-aside, #div-gpt-ad-9092914-1, #div-gpt-ad-9092914-2, .top-ads-boxes, .Campaign").remove();
 // Has Attribute Selector [name]
 // https://api.jquery.com/has-attribute-selector/
-$("div[data-google-query-id]").remove(); // It doesn't work
--- js/website/plantuml.com.js
+++ js/website/plantuml.com.js
@@ -1,1 +1,1 @@
-$("#ad_unit, #e109, #e106, #e171, #ezoic-pub-ad-placeholder-147, .ezoic-ad").remove();
+$("#ad, #ad_unit, #e109, #e106, #e171, #ezoic-pub-ad-placeholder-147, .ezoic-ad, .left ").remove();
